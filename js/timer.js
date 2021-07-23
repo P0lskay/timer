fe38767d79787750a01e1de6f46a0e514b4227da
P0lskay <gg.lol.2264@gmail.com>
2021-07-23 10:28:00 +0300

    Timer Base

--- js/timer.js
+++ js/timer.js
@@ -1,3 +1,23 @@
-let timeInput = document.getElementById("time");
-let buttonStart = document.getElementById("botton");
-let timer = document.getElementById("timer");
+let timerInput = document.getElementById("time");
+let buttonStart = document.getElementById("button");
+let timerShow  = document.getElementById("timer");
+
+
+buttonStart.addEventListener("click", function(){
+    timeMinut = parseInt(timerInput.value) * 60;
+})
+
+timer = setInterval(function(){
+    seconds = timeMinut%60;
+    minuts = timeMinut/60%60;
+    hour = timeMinut/60/60%60;
+
+    if (timeMinut <=0){
+        clearInterval(timer);
+        alert("Time is up");
+    }else{
+        let strTimer = `${Math.trunc(hour)}:${Math.trunc(minuts)}:${seconds}`;
+        timerShow .innerHTML = strTimer;
+    }
+    --timeMinut;
+}, 1000)
