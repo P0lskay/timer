b4468e22391bffe7cd46af723284cf006534740b
P0lskay <gg.lol.2264@gmail.com>
2021-07-29 23:20:58 +0300

    добавлен фон и функция, меняющая его цвет в зависимости от остатка времени. нужно устронить баг с цветом фона и анимировать его изменение

--- js/timer.js
+++ js/timer.js
@@ -2,7 +2,15 @@ let timerInput = document.getElementById("time");
 let buttonStart = document.getElementById("button");
 let timerShow  = document.getElementById("timer");
 
-console.log(this)
+function colorCheck(min){
+    let html = document.getElementsByTagName('html');
+    if (min === 1){
+        html[0].style.background = 'linear-gradient( to top left, #e4eee4, #d8ac08,  #e4eee4, #d8ac08 )';
+    }
+    else if (min < 5){
+        html[0].style.background = 'linear-gradient( to top left, #c83333, #e4eee4, #c83333, #e4eee4)';
+    }
+}
 
 function zeroShow(digit){
     if (digit < 10){
@@ -27,5 +35,6 @@ timer = setInterval(function(){
         let strTimer = `${zeroShow(Math.trunc(hour))}:${zeroShow(Math.trunc(minuts))}:${zeroShow(seconds)}`;
         timerShow .innerHTML = strTimer;
     }
+    colorCheck(timeMinut/60%60);
     --timeMinut;
 }, 1000)
